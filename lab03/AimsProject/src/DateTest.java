12f22b5ac00d8dc0c13fd3fb9c837e2739a8bd99
thangnkHust <thangnk.hust@gmail.com>
2020-03-15 11:56:21 +0700

    Upload AimsProject in lab03

---
+++ lab03/AimsProject/src/DateTest.java
@@ -0,0 +1,29 @@
+
+public class DateTest {
+
+	public static void main(String[] args) {
+		
+//		Test Constructor no parameter 
+		MyDate testDate = new MyDate();
+		System.out.println("Test Case 1: ");
+		testDate.print();
+		
+//		Test Constructor 3 parameter
+		MyDate testDate2 = new MyDate(30, 4, 2019);
+		System.out.println("Test Case 2: ");
+		testDate2.print();
+		
+//		Test Constructor with 1 string parameter 
+		MyDate testDate3 = new MyDate("June 08 2020");
+		System.out.println("Test Case 3: ");
+		testDate3.print();
+		
+//		Test accept method from MyDate Class
+		System.out.println("Test Case 4: ");
+		MyDate testDate4 = new MyDate("");
+		testDate4.print();
+		
+		System.exit(0);
+	}
+
+}
